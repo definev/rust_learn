ed7bab9ad3a90ca9880ff2270c243d2fb49700a4
definev <daiduong.workmail@gmail.com>
2021-12-17 18:19:58 +0700

    add: clarify what enum can do with enum value

--- 6.1 - enums/src/main.rs
+++ 6.1 - enums/src/main.rs
@@ -1,6 +1,6 @@
 enum IpAddrKind {
-    v4(String),
-    v6((u8, u8, u8, u8)),
+    v4(String),           // We can assign any data type with enum
+    v6((u8, u8, u8, u8)), // Type with enum can be different per enum
 }
 
 fn main() {
@@ -10,5 +10,3 @@ fn main() {
     let localhost_v4 = IpAddrKind::v4(String::from("127.0.0.1"));
     let localhost_v6 = IpAddrKind::v6((127, 0, 0, 1));
 }
-
-
